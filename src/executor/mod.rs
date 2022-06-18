edc58424bcf6aeb094c046e1f65070da4b5c1d80
Anurag Singh <contact@anurag.sh>
2022-06-18 17:40:32 +0800

    remove 'static

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -5,5 +5,5 @@ pub(crate) type DynamicExecutor<TContext, TRequest, TResponse> =
     Arc<dyn Executor<TContext, TRequest, TResponse> + Send + Sync>;
 pub use executor::Executor;
 
-pub type ExecutorError = Box<dyn std::error::Error + Send + Sync + 'static>;
+pub type ExecutorError = Box<dyn std::error::Error + Send + Sync>;
 pub type ExecutorResult<T> = Result<T, ExecutorError>;
